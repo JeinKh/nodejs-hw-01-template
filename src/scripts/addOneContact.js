6c9a7c134557ec2e42290747355009d4f7f68580
JeinKh <juliakhezhun@gmail.com>
2024-09-16 22:17:13 +0300

    finish

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,17 @@
-export const addOneContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  const contacts = await readContacts();
+  const newContact = {
+    id: 11,
+    name: 'Julianne',
+    email: 'jbraleya@wikimedia.org',
+    phone: '104-690-8720',
+    job: 'Senior Financial Analyst',
+  };
+  contacts.push(newContact);
+  writeContacts(contacts);
+};
 
 addOneContact();
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,12 @@
-export const removeLastContact = async () => {};
+// import fs from 'node:fs/promises';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+export const removeLastContact = async () => {
+  const contacts = await readContacts();
+
+  const newArr = contacts.slice(1, contacts.length - 1);
+
+  await writeContacts(newArr);
+};
 
 removeLastContact();
